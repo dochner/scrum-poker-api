b5bee5b950994c09ea64ded2395448ddffa6c392
Douglas Ochner <douglas@multiplier.com.br>
2022-02-22 15:02:14 -0300

    feat: add packages and create app

---
+++ README.md
@@ -0,0 +1,1 @@
+# poker-scrum-server
---
+++ app.js
@@ -0,0 +1,96 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const short = require('short-uuid');
+
+require('dotenv').config();
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: process.env.ORIGIN || 'http://localhost:8081',
+    methods: ['GET', 'POST'],
+  }
+});
+
+setInterval(() => {
+  io.emit('ping')
+  logRooms()
+}, 8000)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+console.log('Origem: ', process.env.ORIGIN)
+http.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:3000');
+})
+
+let players = []
+
+function updateClientsInRoom(roomId) {
+  const roomPlayers = players.filter(p => p.roomId == roomId);
+  io.to(roomId).emit('update', roomPlayers);
+}
+
+function restartGame(roomId) {
+  const roomPlayers = players.filter(p => p.roomId == roomId);
+  roomPlayers.forEach(p => p.vote = undefined);
+  console.log('Restarting', roomPlayers);
+  io.to(roomId).emit('restart');
+  io.to(roomId).emit('update', roomPlayers);
+}
+
+function logRooms() {
+  const rooms = players.map(e => e.roomId);
+  if (rooms) {
+    for (const room of rooms.filter((val, i, arr) => arr.indexOf(val) == i)) {
+      const playersInRoom = players.filter(p => p.roomId == room).map(p => p.name);
+      console.log(`Room: ${room} - Players: ${playersInRoom.join(", ")}`);
+    }
+  }
+}
+
+io.on('connection', (socket) => {
+  console.log('User joined room ', socket.id)
+
+  let roomId = socket.handshake.query.roomId
+  if (!roomId) {
+    roomId = short.generate()
+    socket.emit('room', roomId)
+  }
+
+  socket.join(roomId)
+
+  players.push({
+    id: socket.id,
+    name: '',
+    roomId: roomId,
+  })
+
+  socket.on('name', (name) => {
+    let player = players.find(p => p.id === socket.id)
+    if (player) {
+      player.name = name
+    }
+
+    updateClientsInRoom(roomId)
+  })
+
+  socket.on('show', (show) => {
+    io.to(roomId).emit('show')
+  })
+
+  socket.on('restart', () => {
+    restartGame(roomId);
+  });
+
+  socket.on('disconnect', () => {
+    players = players.filter(player => player.id !== socket.id);
+    updateClientsInRoom(roomId);
+  });
+
+  // keeping the connection alive
+  socket.on('pong', () => {
+    let player = players.find(p => p.id == socket.id);
+  })
+})
